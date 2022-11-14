879ab16364c4c75d2f9a4ada29bc377f1532f83e
Xiangbj17 <xiangbj17@mails.tsinghua.edu.cn>
2022-11-14 16:31:56 +0800

    11-14

---
+++ CalSTI.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+"""
+@file      :  CalSTI.py
+@Time      :  2022/11/13 17:44
+@Software  :  PyCharm
+@summary   :
+@Author    :  Bajian Xiang
+"""
+import scipy.signal
+import torch
+import time
+import pandas as pd
+from moviepy.editor import AudioFileClip
+import numpy as np
+from torchaudio.transforms import Resample
+from pathlib import Path
+from model.FpnAttentionEncoder import AttentionFPN
+from wav2spec import wave2spec
+
+OUTPUT_PATH = Path(__file__).parent
+ASSETS_PATH = OUTPUT_PATH / Path("save_model")
+
+
+def relative_to_assets(path: str) -> Path:
+    return ASSETS_PATH / Path(path)
+
+
+model_path = relative_to_assets('19epoch_attention_STI_FPN_Encoder.pt')
+cut_time = 10
+
+def check_audio(wave_data, framerate):
+    if framerate != 16000:
+        wave_data = scipy.signal.resample(wave_data, int(len(wave_data)/framerate*16000))
+    nframes, nchannels = wave_data.shape
+    if nchannels == 2:
+        wave_data = wave_data.T[0]
+    if isinstance(wave_data[0], np.float):
+        wave_data = np.array(wave_data * 32768.0, dtype=np.int16)
+    elif isinstance(wave_data[0], np.int32):
+        wave_data = (wave_data >> 16).astype(np.int16)
+    audio_time = len(wave_data) / 16000
+    return wave_data, audio_time, 16000
+
+
+def load_checkpoint(checkpoint_path=None, trained_epoch=None, model=None, device=None):
+    save_model = torch.load(checkpoint_path, map_location=device)
+    model.load_state_dict(save_model)
+
+    return model
+
+
+def second_to_time(a):
+    if '.5' in str(a):
+        ms = 5000
+        a = int(a - 0.5)
+    else:
+        ms = 0000
+        a = int(a)
+    h = a // 3600
+    m = a // 60 % 60
+    s = a % 60
+    return str("{:0>2}:{:0>2}:{:0>2}.{:0>4}".format(h, m, s, ms))
+
+
+class ResultPrinter(object):
+    def __init__(self):
+        self.out_dict = {'start': [], 'end': [], 'STI': []}
+
+    def print_results(self, out):
+        already_time = self.get_start_len()
+        for i in range(len(out)):
+            start = (already_time + i) * 0.5
+            end = start + 4
+            print('time: [%.1f]s ~ [%.1f]s      STI: [%.4f] ' % (start, end, float(out[i])))
+            self.out_dict['start'].append(start)
+            self.out_dict['end'].append(end)
+            self.out_dict['STI'].append(float(out[i]))
+
+    def save_csv_result(self, save_file_path):
+        out_data = pd.DataFrame.from_dict(self.out_dict)
+        out_data.to_csv(save_file_path)
+        print('Save csv already! Path: ', save_file_path)
+
+    def get_start_len(self):
+        return len(self.out_dict['start'])
+
+    def get_csv_result(self):
+        return pd.DataFrame.from_dict(self.out_dict)
+
+
+def get_offline_res(wave_data, audio_time, framerate, model):
+    print('Load Model...')
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    net = AttentionFPN(num_blocks=[2, 4, 23, 3], num_classes=3, back_bone="resnet50", pretrained=False)
+
+    # check model
+    start_time = time.time()
+    net = load_checkpoint(model_path, 99, net, device)
+    print('Successfully Loaded model: {}'.format(model_path))
+    print('Finished Initialization in {:.3f}s.\n'.format(time.time() - start_time))
+    resultPrinter = ResultPrinter()
+    net.to(device)
+
+    if audio_time <= cut_time:
+        # 25s 以内的音频
+        images, empty_index = wave2spec(wave_data, audio_time, framerate, model)  # tensor:[chunk, 3, 224, 224]
+        print('Available chunks in total: [ ', images.shape[0], ' ] \n')
+
+        with torch.no_grad():
+            net.eval()
+            images_reshape = images.to(torch.float32).to(device)
+            output_pts = net(images_reshape, [1 for _ in range(images_reshape.shape[0])])
+            output_pts = output_pts.squeeze().tolist()
+
+        if empty_index:
+            for item in empty_index:
+                output_pts.insert(item, 'NaN')
+        print('STI Results:')
+        if isinstance(output_pts, float):
+            output_pts = [output_pts]
+        resultPrinter.print_results(out=output_pts)
+        return resultPrinter.get_csv_result()
+
+
+def calSTI(path):
+    """
+    :param path: mp4 文件的地址
+    :return: pd.DataFrame
+    """
+    my_audio_clip = AudioFileClip(path)
+    audio = my_audio_clip.to_soundarray()
+    fs = my_audio_clip.fps
+    audio, audio_time, fs = check_audio(audio, fs)
+    get_offline_res(audio, audio_time, fs, 'attention')
+
+
+if __name__ == "__main__":
+    path = "/Users/bajianxiang/Desktop/你有毒吧.mp4"
+    calSTI(path)
---
+++ generate_sti_video2.py
@@ -0,0 +1,183 @@
+# coding: utf-8
+
+
+import os
+
+import time
+import cv2
+import numpy as np
+import pandas as pd
+from moviepy import *
+from moviepy.editor import *
+import glob
+
+file = pd.read_csv("/Users/queenie/Documents/generate_STIdemo/Temp/csv/伟贤看天下[2]_STI_result_slice4.csv")
+videos_path = "/Users/queenie/Documents/generate_STIdemo/Temp/origin_video/22.mp4"
+frames_save_path = "./伟贤看天下22"
+# image = frames_save_path
+new_video_save_path = "./伟贤看天下22.mp4"
+GENERATE_IMAGE = True
+SAVE_VIDEO_FRAME = False
+
+if not os.path.exists(frames_save_path):
+    os.makedirs(frames_save_path)
+
+if __name__ == "__main__":
+    lst = [x for x in file['STI']]
+    temp = [x for x in lst if pd.isnull(x) == False]
+    sti_value = ['%.2f' % x for x in temp]
+    dem_str = ["STI" + ' ' + x for x in sti_value]
+
+    my_audio_clip = AudioFileClip(videos_path)
+
+    vidcap = cv2.VideoCapture(videos_path)
+    fps = vidcap.get(5)
+    # size = (352,640)
+    size = (int(vidcap.get(3)), int(vidcap.get(4)))
+    print("fps:", fps, "vidsize:", size, "")
+    # success, image = vidcap.read()
+    time_interval = 1
+    # success, image = vidcap.read()
+    success = True
+
+    count = 0
+    if GENERATE_IMAGE == True:
+        while success:
+            success, image = vidcap.read()
+            count += 1
+            start = time.time()
+
+            if count % time_interval == 0:
+                try:
+                    if os.path.exists(os.path.join(frames_save_path, "frame%d.jpg" % (count))):
+                        continue
+                    else:
+                        cv2.imencode('.jpg', image)[1].tofile(os.path.join(frames_save_path, "frame%d.jpg" % (count)))
+                except:
+                    continue
+
+    image_lst = os.listdir(frames_save_path)
+
+    b = 1
+    d = 0
+    j = 0
+    MP4_format = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+    video = cv2.VideoWriter(new_video_save_path, MP4_format, fps, size)
+    for i in range(len(image_lst)):
+        image_path = os.path.join(frames_save_path, 'frame%d.jpg' % (d + 1))
+        d = d + 1
+        text = dem_str[j]
+        try:
+            src = cv2.imread(image_path)
+        except:
+            continue
+        temp = cv2.putText(src, text, (25, 40), cv2.FONT_HERSHEY_COMPLEX, 1.2, (0, 0, 0), 2)
+        STRIDE = 2
+        bar_start_x = 10
+        bar_start_y = 50
+        bar_height = 20
+
+        bad_thresh = 30
+        poor_thresh = 45
+        fair_thresh = 60
+        good_thresh = 75
+        excellent_thresh = 100
+
+        bar_bad = bar_start_x + 30 * STRIDE
+        bar_poor = bar_start_x + 45 * STRIDE
+        bar_fair = bar_start_x + 60 * STRIDE
+        bar_good = bar_start_x + 75 * STRIDE
+        bar_excellent = bar_start_x + 100 * STRIDE
+        sti_value = float(text.split(" ")[-1])
+        sti_value_large_100 = 100.0 * sti_value
+
+        if sti_value_large_100 < bad_thresh:
+            temp = cv2.rectangle(temp, (bar_start_x, bar_start_y),
+                                 (bar_start_x + sti_value_large_100 * STRIDE, bar_height + bar_start_y), (128, 0, 128),
+                                 thickness=-1)
+        elif bad_thresh <= sti_value_large_100 and sti_value_large_100 <= poor_thresh:
+            temp = cv2.rectangle(temp, (bar_start_x, bar_start_y), (bar_bad, bar_height + bar_start_y), (128, 0, 128),
+                                 thickness=-1)
+            temp = cv2.rectangle(temp, (bar_bad, bar_start_y),
+                                 (int(bar_bad + (sti_value_large_100 - bad_thresh) * STRIDE), bar_height + bar_start_y),
+                                 (0, 0, 255),
+                                 thickness=-1)
+            # print((bar_bad, bar_start_y))
+            # print((bar_bad + sti_value_large_100 * STRIDE, bar_height + bar_start_y))
+            # cv2.imshow("origin",temp)
+            # cv2.waitKey()
+
+        elif poor_thresh <= sti_value_large_100 and sti_value_large_100 <= fair_thresh:
+            temp = cv2.rectangle(temp, (bar_start_x, bar_start_y), (bar_bad, bar_height + bar_start_y), (128, 0, 128),
+                                 thickness=-1)
+            temp = cv2.rectangle(temp, (bar_bad, bar_start_y), (bar_poor, bar_height + bar_start_y), (0, 0, 255),
+                                 thickness=-1)
+            temp = cv2.rectangle(temp, (bar_poor, bar_start_y), (
+            bar_poor + int((sti_value_large_100 - poor_thresh) * STRIDE), bar_height + bar_start_y), (0, 69, 255),
+                                 thickness=-1)
+
+        elif fair_thresh <= sti_value_large_100 and sti_value_large_100 <= good_thresh:
+            temp = cv2.rectangle(temp, (bar_start_x, bar_start_y), (bar_bad, bar_height + bar_start_y), (128, 0, 128),
+                                 thickness=-1)
+            temp = cv2.rectangle(temp, (bar_bad, bar_start_y), (bar_poor, bar_height + bar_start_y), (0, 0, 255),
+                                 thickness=-1)
+            temp = cv2.rectangle(temp, (bar_poor, bar_start_y), (bar_fair, bar_height + bar_start_y), (0, 69, 255),
+                                 thickness=-1)
+            temp = cv2.rectangle(temp, (bar_fair, bar_start_y), (
+            bar_fair + int((sti_value_large_100 - fair_thresh) * STRIDE), bar_height + bar_start_y), (0, 255, 255),
+                                 thickness=-1)
+        else:
+            temp = cv2.rectangle(temp, (bar_start_x, bar_start_y), (bar_bad, bar_height + bar_start_y), (128, 0, 128),
+                                 thickness=-1)
+            temp = cv2.rectangle(temp, (bar_bad, bar_start_y), (bar_poor, bar_height + bar_start_y), (0, 0, 255),
+                                 thickness=-1)
+            temp = cv2.rectangle(temp, (bar_poor, bar_start_y), (bar_fair, bar_height + bar_start_y), (0, 69, 255),
+                                 thickness=-1)
+            temp = cv2.rectangle(temp, (bar_fair, bar_start_y), (bar_good, bar_height + bar_start_y), (0, 255, 255),
+                                 thickness=-1)
+            temp = cv2.rectangle(temp, (bar_good, bar_start_y), (
+            bar_good + int((sti_value_large_100 - good_thresh) * STRIDE), bar_height + bar_start_y), (0, 255, 0),
+                                 thickness=-1)
+
+        # temp = cv2.rectangle(temp, (bar_start_x, bar_start_y), (bar_bad, bar_height + bar_start_y), (128,0,128), thickness=-1)
+        # temp = cv2.rectangle(temp, (bar_bad, bar_start_y), (bar_poor, bar_height + bar_start_y), (0,0,255), thickness=-1)
+        # temp = cv2.rectangle(temp, (bar_poor, bar_start_y), (bar_fair, bar_height + bar_start_y), (0,69,255), thickness=-1)
+        # temp = cv2.rectangle(temp, (bar_fair, bar_start_y), (bar_good, bar_height + bar_start_y), (0,255,255), thickness=-1)
+        # temp = cv2.rectangle(temp, (bar_good, bar_start_y), (bar_excellent, bar_height + bar_start_y), (0,255,0), thickness=-1)
+
+        # indicator_x = bar_start_x + sti_value * 100
+        # indicator_y = bar_start_y + 0.5 * bar_height
+
+        # temp = cv2.circle(temp, (int(indicator_x),int(indicator_y)), int(0.5 * bar_height), (0,0,0), thickness=-1)
+
+        # cv2.imshow("temp",temp)
+        # cv2.waitKey()
+        if not src is None and SAVE_VIDEO_FRAME == True:
+            cv2.imwrite(os.path.join(frames_save_path, "frame%d.png" % (d)), src)
+        # cv2.imwrite(r'C:\Users\17579\Desktop\Yousonic\STI_Video_Demo\Self\Image\down\frame%d.jpg'%d,src)
+        b = b + 1
+        if b % 30 == 0:
+            j = j + 1
+            # print("11111")
+        video.write(src)
+    video.release()
+
+    video = VideoFileClip(new_video_save_path)
+    video = video.set_audio(my_audio_clip)
+    # video.write_videofile(new_video_save_path.split("/")[-1].split(".")[0]+"_audio.mp4",\
+    #                       codec='png',audio_codec='aac',temp_audiofile='temp-audio.m4a',remove_temp=True)
+    video.write_videofile(new_video_save_path.split("/")[-1].split(".")[0] + "_audio.mp4", \
+                          codec='libx264', audio_codec='aac', temp_audiofile='temp-audio.m4a', remove_temp=True)
+
+    # image_lst = os.listdir(frames_save_path)
+    #
+    #
+    #
+    #
+    #
+    #
+    #
+    # for i in range(len(image_lst)):
+    #     item = os.path.join(frames_save_path,'frame%d.png' %(i))
+    #     pictur = cv2.imread(item)
+    #
